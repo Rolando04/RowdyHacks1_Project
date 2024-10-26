d70c82402f27ebe7faa8e8aa6dddd1110e84ece6
ChronicAtom533 <Rolandoecastrellon@gmail.com>
2024-10-26 17:54:44 -0500

    Score Keeping

---
+++ RowdyHacks-X/Assets/Scripts/scoreKeeper.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scoreKeeper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static scoreKeeper instance;
+    public Text scoreText;
+    public Text highScore;
+    public Text lives;
+    int score = 0;
+    int highS = 0;
+    int life = 3;
+    private void Awake(){
+        instance = this;
+    }
+    void Start()
+    {
+        highS = PlayerPrefs.GetInt("highscore", 0);
+        scoreText.text = "Score: " + score.ToString();
+        highScore.text = "Highscore: " + score.ToString();
+        lives.text = "Lives: x" + score.ToString();
+    }
+
+    // Update is called once per frame
+    public void AddPoints(){
+        score += 1;
+        scoreText.text = "Score: " + score.ToString();
+        if(highS < score)
+            PlayerPrefs.SetInt("highscore", score);
+
+    }
+}
